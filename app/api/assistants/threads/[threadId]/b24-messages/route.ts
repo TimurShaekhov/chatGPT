d3cdb664f7ab2084fd8969658074d67105b5e7f6
Timur Shaekhov <prospektweb@gmail.com>
2024-09-25 14:38:25 +0500

    Update route.ts

--- app/api/assistants/threads/[threadId]/b24-messages/route.ts
+++ app/api/assistants/threads/[threadId]/b24-messages/route.ts
@@ -15,44 +15,30 @@ export async function POST(request, { params: { threadId } }) {
     content: data.content.toString(),
   });
 
-  let run = await openai.beta.threads.runs.create(threadId, { assistant_id: data.assistantId });
-  
+  let run = await openai.beta.threads.runs.create( threadId, { assistant_id: data.assistantId } );
+ 
   let attempts = 0;
   while (attempts < 10) {
     run = await openai.beta.threads.runs.retrieve(threadId, run.id);
-    
     if (run.status === "completed") {
-      const messages = await openai.beta.threads.messages.list(threadId);
-      const latestMessage = messages.data[0];
-      
-      return new Response(
-        JSON.stringify({
-          thread_id: threadId,
-          run_id: run.id,
-          message: latestMessage,
-          data: data,
-          status: 'completed',
-        }),
-        { headers: { 'Content-Type': 'application/json' } }
-      );
+        const messages = await openai.beta.threads.messages.list(threadId);
+        const latestMessage = messages.data[0];
+        
+        return new Response(
+          JSON.stringify({            
+            thread_id: threadId,
+            run_id: run.id,
+            message: latestMessage,
+            data: data,
+            status: 'completed',
+          }),
+          { headers: { 'Content-Type': 'application/json' }, }
+        )
     }
-
-    if (["requires_action",
-       "cancelling",
-       "cancelled",
-       "failed",
-       "expired",
-       ].includes(run.status)) {
-      console.log('Run has problems. Exiting the loop.');
-      break;
-    }
-
+    
     await new Promise(resolve => setTimeout(resolve, 1000));
     attempts++;
   }
 
-  return new Response(
-    JSON.stringify({data: data, status: 'failed'}),
-    { headers: { 'Content-Type': 'application/json' } }
-  );
+  return { thread_id: threadId, run_id: run.id, status: 'failed' };
 }
