7381236162c9d637e70fe2ff34ed5e0da009cc5a
Timur Shaekhov <prospektweb@gmail.com>
2024-09-25 12:50:30 +0500

    Правки

--- app/api/assistants/threads/[threadId]/b24-messages/route.ts
+++ app/api/assistants/threads/[threadId]/b24-messages/route.ts
@@ -4,12 +4,12 @@ import { openai } from "@/app/openai";
 export const runtime = "nodejs";
 
 // Send a new message to a thread
-export async function POST(request) {
+export async function POST(request, { params: { threadId } }) {
   const data = await request.json();
 
-  await openai.beta.threads.messages.create(data.threadId, {
-    role: data.user,
-    content: data.content,
+  await openai.beta.threads.messages.create(threadId, {
+    role: data.role,
+    content: (data.content).toString(),
   });
 
   const stream = openai.beta.threads.runs.stream(data.threadId, {
