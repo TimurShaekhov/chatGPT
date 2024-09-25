89e7712ca35eea51994411f314c788a3e43b8806
Timur Shaekhov <prospektweb@gmail.com>
2024-09-25 15:17:20 +0500

    Update route.ts

--- app/api/assistants/threads/[threadId]/b24-messages/route.ts
+++ app/api/assistants/threads/[threadId]/b24-messages/route.ts
@@ -1,4 +1,3 @@
-import { assistantId } from "@/app/assistant-config";
 import { openai } from "@/app/openai";
 
 export const runtime = "nodejs";
@@ -6,14 +5,6 @@ export const runtime = "nodejs";
 // Send a new message from other SERVER to a thread
 export async function POST(request, { params: { threadId } }) {
   const data = await request.json();
-
-  return new Response(
-    JSON.stringify({
-      data: data,
-      status: 'test',
-    }),
-    { headers: { 'Content-Type': 'application/json' }, }
-  )
 
   if (!data.content) {
     throw new Error('Content is required. Received data:', data);
@@ -24,33 +15,44 @@ export async function POST(request, { params: { threadId } }) {
     content: data.content.toString(),
   });
 
-  let run = await openai.beta.threads.runs.create( threadId, { assistant_id: data.assistantId } );
- 
+  let run = await openai.beta.threads.runs.create(threadId, { assistant_id: data.assistantId });
+  
   let attempts = 0;
-  while (attempts < 30) {
+  while (attempts < 90) {
     run = await openai.beta.threads.runs.retrieve(threadId, run.id);
-    if (run.status === "completed") {
-        const messages = await openai.beta.threads.messages.list(threadId);
-        const latestMessage = messages.data[0];
-        
-        return new Response(
-          JSON.stringify({
-            thread_id: threadId,
-            run_id: run.id,
-            message: latestMessage,
-            data: data,
-            status: 'completed',
-          }),
-          { headers: { 'Content-Type': 'application/json' }, }
-        )
-    }
     
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    if (run.status === "completed") {
+      const messages = await openai.beta.threads.messages.list(threadId);
+      const latestMessage = messages.data[0];
+      
+      return new Response(
+        JSON.stringify({
+          thread_id: threadId,
+          run_id: run.id,
+          message: latestMessage,
+          data: data,
+          status: 'completed',
+        }),
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+    }
+
+    if (["requires_action",
+       "cancelling",
+       "cancelled",
+       "failed",
+       "expired",
+       ].includes(run.status)) {
+      console.log('Run has problems. Exiting the loop.');
+      break;
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
     attempts++;
   }
 
   return new Response(
-    JSON.stringify({ data: data, status: 'failed' }),
-    { headers: { 'Content-Type': 'application/json' }, }
-  )
+    JSON.stringify({data: data, status: 'failed'}),
+    { headers: { 'Content-Type': 'application/json' } }
+  );
 }
